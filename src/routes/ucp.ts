65bd49721ad02b6c361b43c925d979680a511193
Tomas Zijdemans <113360400+tomas-zijdemans@users.noreply.github.com>
2026-02-22 16:19:10 +0100

    cache-control (#9)

--- src/routes/ucp.ts
+++ src/routes/ucp.ts
@@ -1,5 +1,17 @@
+import { formatCacheControl } from "@std/http/unstable-cache-control";
 import { ucpProfile } from "../data/ucp-profile.ts";
 
+const PROFILE_CACHE_CONTROL = formatCacheControl({
+  public: true,
+  maxAge: 300,
+  staleWhileRevalidate: 60,
+});
+
 export function handleGetUCPProfile(_req: Request): Response {
-  return new Response(JSON.stringify(ucpProfile));
+  return new Response(JSON.stringify(ucpProfile), {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": PROFILE_CACHE_CONTROL,
+    },
+  });
 }
